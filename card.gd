b1a5be039290ad1a981bcf1cb5f3a949e5189685
sos2005 <estebangetio@gmail.com>
2026-03-15 11:36:16 -0500

    Create card.gd

---
+++ card.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+signal hovered
+signal hovered_off
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#get_parent().connect_card_signals(self)
+	pass
+	
+
+
+func _on_area_2d_mouse_entered() -> void:
+	emit_signal("hovered", self)
+
+
+func _on_area_2d_mouse_exited() -> void:
+	emit_signal("hovered_off", self)
