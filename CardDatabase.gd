8728fe25b516fe8670bc50a84ced57aafd9fc744
JuanPabloSotelo <55117590+JuanPabloSotelo@users.noreply.github.com>
2026-03-23 12:21:19 -0500

    Añadido tipo de carta al database CardDatabase.gd

--- CardDatabase.gd
+++ CardDatabase.gd
@@ -1,7 +1,8 @@
-const Cards = { #Ataque, Vida, 
-	"Hannah":[1 , 1],
-	"Cylindrus":[3 , 7],
-	"Pierre":[2,3],
-	"Aku":[5,4],
-	"Drain":[5,5]
+const Cards = { #Ataque, Vida, tipo de carta
+	"Hannah":[1 , 1,"Tropa"],
+	"Cylindrus":[3 , 7,"Entorno"],
+	"Pierre":[2,3,"Support_campo"],
+	"Aku":[5,4,"Truco_linea"],
+	"Drain":[5,5,"Truco_campo"],
+	"OrdenSuprema": [null,null,"Support_linea"]
 }
