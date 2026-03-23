4df5b0b08d36bb217139853b87d2f01e5d8de121
JuanPabloSotelo <55117590+JuanPabloSotelo@users.noreply.github.com>
2026-03-23 12:16:07 -0500

    Identificador de tipo de cartas y muestreo de slot dependiendo del tipo de carta card_manager.gd

--- card_manager.gd
+++ card_manager.gd
@@ -18,10 +18,22 @@ var is_dragging = false
 var card_preview_reference
 #-------------
 
+#verificador de cartas
+var slots_tropa
+var slots_entorno
+var slots_linea
+var slots_campo
+#-----------
+
 
 #guarda tamaño dela ventana asi la carta no se saldra  de la pantalla
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	slots_tropa = $"../SlotManager/SlotTropa"
+	slots_entorno = $"../SlotManager/SlotEntorno"
+	slots_linea = $"../SlotManager/SlotLinea"
+	slots_campo = $"../SlotManager/SlotCampo"
+	toggle_slots_visibility("", false)
 	player_hand_reference = $"../PlayerHand"
 	card_preview_reference = $"../CanvasLayer/CardPreview"
 	$"../InputManager".connect("left_mouse_button_released", on_left_click_released)
@@ -49,33 +61,94 @@ func start_drag(card):
 	click_timer = 0.0
 	is_dragging = false
 	card.scale = CARD_BASE_SCALE
-	toggle_slots_visibility(true)
+	toggle_slots_visibility(card.card_type, true)
 	
 
 func finish_drag():
 	if card_being_dragged:
 		card_being_dragged.scale = CARD_BASE_SCALE
 		var card_slot_found = raycast_check_for_card_slot()
-		if card_slot_found and not card_slot_found.card_in_slot:
-			card_being_dragged.position = card_slot_found.position
-			player_hand_reference.remove_card_from_hand(card_being_dragged)
-			card_being_dragged.get_node("Area2D").collision_layer = 8
-			card_being_dragged.card_preview_reference = card_preview_reference
-			#indica cuando la carta se puso correctamente en un slot
-			card_being_dragged.in_slot = true 
-			card_being_dragged.scale = CARD_BASE_SCALE * 0.55
-			card_slot_found.card_in_slot = true
-			card_being_dragged.card_preview_reference = card_preview_reference
+		var card_type = card_being_dragged.card_type
+		if card_slot_found and not card_slot_found.card_in_slot and card_type in card_slot_found.card_slot_type:
+			if (card_type == "Support_linea" or card_type == "Support_campo") and played_support_card:
+				print("Ya jugaste un support este turno")
+				player_hand_reference.add_card_to_hand(card_being_dragged, DEFAULT_CARD_MOVE_SPEED)
+			else:
+				card_being_dragged.position = card_slot_found.position
+				player_hand_reference.remove_card_from_hand(card_being_dragged)
+				card_being_dragged.get_node("Area2D").collision_layer = 8
+				card_being_dragged.card_preview_reference = card_preview_reference
+				#indica cuando la carta se puso correctamente en un slot
+				
+				card_being_dragged.in_slot = true 
+				card_being_dragged.scale = CARD_BASE_SCALE * 0.7
+				card_slot_found.card_in_slot = true
+				card_being_dragged.card_preview_reference = card_preview_reference
+				if card_type == "Support_linea" or card_type == "Support_campo":
+					played_support_card = true
 		else:
 			player_hand_reference.add_card_to_hand(card_being_dragged, DEFAULT_CARD_MOVE_SPEED)
-		toggle_slots_visibility(false)
+		toggle_slots_visibility(card_type, false)
 		card_being_dragged = null
 	
-func toggle_slots_visibility(visible):
-	for child in get_parent().get_children():
-		if child.has_method("get") and child.get("card_in_slot") != null:
-			if not child.card_in_slot:
-				child.visible = visible
+func toggle_slots_visibility(card_type, show):
+	if slots_tropa:
+		slots_tropa.visible = false
+		for slot in slots_tropa.get_children():
+			if slot.has_node("Area2D"):
+				slot.get_node("Area2D").set_collision_layer_value(2, false)
+				slot.get_node("AnimationPlayer").play("slot_in")
+	if slots_entorno:
+		slots_entorno.visible = false
+		for slot in slots_entorno.get_children():
+			if slot.has_node("Area2D"):
+				slot.get_node("Area2D").set_collision_layer_value(2, false)
+				slot.get_node("AnimationPlayer").play("slot_in")
+	if slots_linea:
+		slots_linea.visible = false
+		for slot in slots_linea.get_children():
+			if slot.has_node("Area2D"):
+				slot.get_node("Area2D").set_collision_layer_value(2, false)
+				slot.get_node("AnimationPlayer").play("slot_in")
+	if slots_campo:
+		slots_campo.visible = false
+		for slot in slots_campo.get_children():
+			if slot.has_node("Area2D"):
+				slot.get_node("Area2D").set_collision_layer_value(2, false)
+				slot.get_node("AnimationPlayer").play("slot_in")
+	if not show:
+		return
+
+	# Muestra solo el grupo correspondiente
+	match card_type:
+		"Tropa":
+			if slots_tropa:
+				slots_tropa.visible = true
+				for slot in slots_tropa.get_children():
+					slot.visible = true
+					if slot.has_node("Area2D"):
+						slot.get_node("Area2D").set_collision_layer_value(2, true)
+		"Entorno":
+			if slots_entorno:
+				slots_entorno.visible = true
+				for slot in slots_entorno.get_children():
+					slot.visible = true
+					if slot.has_node("Area2D"):
+						slot.get_node("Area2D").set_collision_layer_value(2, true)
+		"Truco_linea", "Support_linea":
+			if slots_linea:
+				slots_linea.visible = true
+				for slot in slots_linea.get_children():
+					slot.visible = true
+					if slot.has_node("Area2D"):
+						slot.get_node("Area2D").set_collision_layer_value(2, true)
+		"Truco_campo", "Support_campo":
+			if slots_campo:
+				slots_campo.visible = true
+				for slot in slots_campo.get_children():
+					slot.visible = true
+					if slot.has_node("Area2D"):
+						slot.get_node("Area2D").set_collision_layer_value(2, true)
 
 func connect_card_signals(card):
 	card.connect("hovered", on_hovered_over_card)
@@ -135,8 +208,10 @@ func raycast_check_for_card_slot():
 	parameters.collision_mask = COLLISION_MASK_CARD_slot
 	var result = space_state.intersect_point(parameters)
 	if result.size() > 0:
-		
-		return result[0].collider.get_parent()
+		for r in result:
+			var parent = r.collider.get_parent()
+			if parent.get("card_slot_type") != null:
+				return parent
 	return null
 
 func get_card_with_highest_z_index(cards):
